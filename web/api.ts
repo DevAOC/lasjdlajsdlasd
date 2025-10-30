50730f01e0ae56a473c6f29d32efe64dbfaacc78
Antoine Charette <antoineocharette@gmail.com>
2025-10-30 13:35:55 -0400

    add all

---
+++ web/api.ts
@@ -0,0 +1,5 @@
+// Sets up the API client for interacting with your backend.
+// For your API reference, visit: https://docs.gadget.dev/api/ajsdkjasdjaklsjdlkajdj
+import { Client } from "@gadget-client/ajsdkjasdjaklsjdlkajdj";
+
+export const api = new Client();
